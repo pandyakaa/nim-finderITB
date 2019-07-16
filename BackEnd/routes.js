e131c1ebddbf36b510712d3c411704a54db85729
pandyakaa <pandyaka.aptanagi@gmail.com>
2019-07-16 15:47:52 +0700

    second

---
+++ BackEnd/routes.js
@@ -0,0 +1,9 @@
+module.exports = function(app) {
+    var todoList = require('./controller');
+
+    app.route('/')
+        .get(todoList.index);
+
+    app.route('/users')
+        .get(todoList.users);
+};
--- Connector/connector.py
+++ Connector/connector.py
@@ -9,7 +9,16 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-sql = "INSERT into nimdb (nimJur,nimTPB,name) VALUES (%s,%s,%s)"
-val = [
-    
-]
+with open("res.txt","r") as f :
+    for l in f :
+        data = [x.strip() for x in l.split("\t")[0].split(",")] + [l.split("\t")[1].strip()]
+        if len(data) == 3 :
+            sql = "INSERT into nimdb (nim_tpb,nim_jur,name) VALUES (%s,%s,%s)"
+        elif len(data) == 2 :
+            sql = "INSERT into nimdb (nim_tpb,name) VALUES (%s,%s)"
+        mycursor.execute(sql,data)
+
+mydb.commit()
+
+mycursor.close()
+mydb.close()
